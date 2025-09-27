bc0a49841f26534252e38c7804b288562480c147
fitsumhelina <dev.fitsum@gmail.com>
2025-09-27 15:13:33 +0300

    feat: Generate Convex API and server utilities, including type definitions for queries, mutations, and data models

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,44 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as about from "../about.js";
+import type * as messages from "../messages.js";
+import type * as projects from "../projects.js";
+import type * as skills from "../skills.js";
+import type * as users from "../users.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  about: typeof about;
+  messages: typeof messages;
+  projects: typeof projects;
+  skills: typeof skills;
+  users: typeof users;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
